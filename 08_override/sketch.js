3a6d8f1dc3a8956b8f1f2d7184a28e7d56073720
IreneeL <yl6447@nyu.edu>
2020-04-05 19:37:18 -0400

    added override chrome extension

---
+++ 08_override/sketch.js
@@ -0,0 +1,19 @@
+// A2Z F17
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F17
+
+// This just picks a random word from wordnik
+// And shows it to the user each time a new tab is opened
+
+// A random word
+
+
+function setup(){
+  noCanvas();
+  let n = floor(random(3)) + 1;
+  let img = createImg('pic/' + n + '.png');
+  img.size(windowsWidth,windowsHeight);
+  img.position(100,100);
+
+}
